a7f80cc39f38dd9deedd41b093054ba336691bc7
Nasir Nooruddin <nasir@nasir.id>
2025-04-22 22:08:17 +0700

    Update elk.sh

--- elk.sh
+++ elk.sh
@@ -55,4 +55,35 @@ sudo ln -s /etc/nginx/sites-available/elk.nasir.id /etc/nginx/sites-enabled/elk.
 sudo nginx -t
 sudo systemctl reload nginx
 
+# install logstatsh
+sudo apt install logstash
+sudo tee /etc/logstash/conf.d/02-beats-input.conf > /dev/null << 'EOT'
+input {
+  beats {
+    port => 5044
+  }
+}
+EOT
+sudo tee /etc/logstash/conf.d/30-elasticsearch-output.conf > /dev/null << 'EOT'
+output {
+  if [@metadata][pipeline] {
+	elasticsearch {
+  	hosts => ["localhost:9200"]
+  	manage_template => false
+  	index => "%{[@metadata][beat]}-%{[@metadata][version]}-%{+YYYY.MM.dd}"
+  	pipeline => "%{[@metadata][pipeline]}"
+	}
+  } else {
+	elasticsearch {
+  	hosts => ["localhost:9200"]
+  	manage_template => false
+  	index => "%{[@metadata][beat]}-%{[@metadata][version]}-%{+YYYY.MM.dd}"
+	}
+  }
+}
+EOT
+sudo -u logstash /usr/share/logstash/bin/logstash --path.settings /etc/logstash -t
+sudo systemctl start logstash
+sudo systemctl enable logstash
+
 
