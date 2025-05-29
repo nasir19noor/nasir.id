56f0cbc8028c0308190b9bcc292c930243d67542
Nasir Noor <nasir@nasir.id>
2025-05-29 12:42:14 +0700

    aws vpc

--- aws/services/vpc/locals.tf
+++ aws/services/vpc/locals.tf
@@ -1,11 +1,11 @@
 locals {
-  config                  = yamldecode(file("../../config.yaml"))
-  cidr_block              = local.aws.config.network.cidr_block
-  region                  = local.aws.config.global.region
-  name                    = local.aws.config.network.vpc_name
-  description             = local.aws.config.network.description
-  instance_tenancy        = local.aws.config.network.instance_tenancy
-  enable_dns_hostnames    = local.aws.config.network.enable_dns_hostnames
-  enable_dns_support      = local.aws.config.network.enable_dns_support
+  config                  = yamldecode(file("../../../config.yaml"))
+  cidr_block              = local.config.aws.network.cidr_block
+  region                  = local.config.aws.global.region
+  name                    = local.config.aws.network.vpc_name
+  description             = local.config.aws.network.description
+  instance_tenancy        = local.config.aws.network.instance_tenancy
+  enable_dns_hostnames    = local.config.aws.network.enable_dns_hostnames
+  enable_dns_support      = local.config.aws.network.enable_dns_support
 }    
 
