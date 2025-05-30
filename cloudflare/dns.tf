908c5b8395491ad86ea6c78b55063618d87eec20
Nasir Noor <nasir@nasir.id>
2025-05-30 20:32:36 +0700

    add test.nasir.id

--- cloudflare/dns.tf
+++ cloudflare/dns.tf
@@ -71,3 +71,12 @@ resource "cloudflare_record" "n8n" {
   ttl     = 300
   proxied = false
 }
+
+resource "cloudflare_record" "test" {
+  zone_id = data.cloudflare_zones.nasir_id.zones[0].id
+  name    = "test"
+  content = local.contabo_ip
+  type    = "A"
+  ttl     = 300
+  proxied = false
+}
