4ebded3a8c0f6ea6184688b96998ab3af2bc5009
Nasir Noor <nasir@nasir.id>
2025-05-31 11:44:22 +0700

    update s3 public access policy

--- aws/services/s3/main.tf
+++ aws/services/s3/main.tf
@@ -1,6 +1,12 @@
 module "s3_wordpress" {
   source = "git::https://github.com/nasir19noor/terraform.git//aws/modules/s3"
   bucket = local.bucket_name_wordpress
+
+  # Disable all public access blocking to allow public access
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 
