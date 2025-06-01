9f7e9305971f9dcd0a72860aab46b0243eaac0e8
Nasir Noor <nasir@nasir.id>
2025-06-01 22:20:51 +0700

    add s3 bucket upload.nasir.id

--- aws/services/s3/locals.tf
+++ aws/services/s3/locals.tf
@@ -3,6 +3,6 @@ locals {
   region                  = local.config.aws.global.region
   bucket_name_wordpress   = local.config.aws.s3.bucket_name_wordpress
   bucket_name_backup      = local.config.aws.s3.bucket_name_backup
-
+  bucket_name_upload      = local.config.aws.s3.bucket_name_upload
 }    
 
--- aws/services/s3/main.tf
+++ aws/services/s3/main.tf
@@ -13,3 +13,14 @@ module "s3_backup" {
   source = "git::https://github.com/nasir19noor/terraform.git//aws/modules/s3"
   bucket = local.bucket_name_backup
 }
+
+module "s3_upload" {
+  source = "git::https://github.com/nasir19noor/terraform.git//aws/modules/s3"
+  bucket = local.bucket_name_upload
+
+  # Disable all public access blocking to allow public access
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
