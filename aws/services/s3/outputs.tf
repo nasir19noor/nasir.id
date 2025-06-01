be30f33b5d67cdb924556586d89a472030370e9d
Nasir Noor <nasir@nasir.id>
2025-06-01 21:54:39 +0700

    add outputss3 bucket backup.nasir.id

--- aws/services/s3/outputs.tf
+++ aws/services/s3/outputs.tf
@@ -4,3 +4,10 @@ output "s3_wordpress_id" {
 output "s3_wordpress_arn" {
   value = module.s3_wordpress.s3_bucket_arn
 }
+
+output "s3_backup_id" {
+  value = module.s3_backup.s3_bucket_id
+}
+output "s3_backup_arn" {
+  value = module.s3_backup.s3_bucket_arn
+}
