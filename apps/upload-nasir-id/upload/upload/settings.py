12c9b7358a3383bc605ef045af1a62d4947a3a97
Nasir Noor <nasir@nasir.id>
2025-06-02 22:16:51 +0700

    update

--- apps/upload-nasir-id/upload/upload/settings.py
+++ apps/upload-nasir-id/upload/upload/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'uploadnasir'
+]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
 
 MIDDLEWARE = [
