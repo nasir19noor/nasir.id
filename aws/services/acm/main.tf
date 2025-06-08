4728f7389737559dfb8229b475594a363d36167c
Nasir Noor <nasir@nasir.id>
2025-06-08 11:19:28 +0700

    update acm assets

--- aws/services/acm/main.tf
+++ aws/services/acm/main.tf
@@ -2,10 +2,5 @@ module "acm_assets" {
   provider    = aws.acm_provider
   source      = "git::https://github.com/nasir19noor/terraform.git//aws/modules/acm"
   domain_name = local.domain_name_assets
-
-  tags = {
-    Name = local.domain_name_assets
-  }
-
 }
 
