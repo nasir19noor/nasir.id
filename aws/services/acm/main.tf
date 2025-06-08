1bac2121a237f18dc147f36d3e06f1b248f9bbc3
Nasir Noor <nasir@nasir.id>
2025-06-08 11:05:40 +0700

    acm assets.nasir.id

--- aws/services/acm/main.tf
+++ aws/services/acm/main.tf
@@ -1,6 +1,15 @@
-module "s3_wordpress" {
-  provider = aws.acm_provider
-  source   = "git::https://github.com/nasir19noor/terraform.git//aws/modules/acm"
-  bucket   = local.domain_name_assets
+module "acm_assets" {
+  provider    = aws.acm_provider
+  source      = "git::https://github.com/nasir19noor/terraform.git//aws/modules/acm"
+  domain_name = local.domain_name_assets
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = local.domain_name_assets
+  }
+
 }
 
