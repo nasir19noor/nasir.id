23fe3777dd1b90c55c3b24ea26eb36dd96c826bd
Nasir Noor <nasir@nasir.id>
2025-06-08 11:32:30 +0700

    acm assets_nasir_id

--- aws/services/acm/locals.tf
+++ aws/services/acm/locals.tf
@@ -2,5 +2,8 @@ locals {
   config                  = yamldecode(file("../../../config.yaml"))
   region                  = local.config.aws.global.region
   domain_name_assets      = local.config.aws.acm.domain_name_assets
+  domain_name             = local.config.aws.acm.domain_name_assets
+  create_route53_records  = local.config.aws.acm.create_route53_records
+  validation_method       = local.config.aws.acm.validation_method
 }    
 
--- aws/services/acm/main.tf
+++ aws/services/acm/main.tf
@@ -1,6 +1,12 @@
 module "acm_assets" {
-  provider    = aws.acm_provider
-  source      = "git::https://github.com/nasir19noor/terraform.git//aws/modules/acm"
-  domain_name = local.domain_name_assets
-}
+  source = "git::https://github.com/nasir19noor/terraform.git//aws/modules/acm"
 
+  domain_name = local.domain_name_assets
+  subject_alternative_names = [
+    "${local.domain_name_assets}"
+  ]
+
+  create_route53_records = local.create_route53_records
+  validation_method      = local.validation_method
+  wait_for_validation    = false
+}
