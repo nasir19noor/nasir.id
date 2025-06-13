a21c52b0a4f385ca0c3598967aded6fc85b5b4e3
Nasir Noor <nasir@nasir.id>
2025-06-13 22:20:27 +0700

    delete s3 bucket website.nasir.id

--- aws/services/s3/main.tf
+++ aws/services/s3/main.tf
@@ -29,17 +29,4 @@ module "s3_upload" {
   enable_public_read_access = true
 }
 
-module "s3_website" {
-  source = "git::https://github.com/nasir19noor/terraform.git//aws/modules/s3"
-  bucket = "website.nasir.id"
 
-  # Disable all public access blocking to allow public access
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-
-  # --- Enable Website and Public Access Features ---
-  enable_website_hosting    = true
-  enable_public_read_access = true
-}
