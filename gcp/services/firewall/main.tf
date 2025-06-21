4f45746daabcedd518519337695e48dbab8d7987
Nasir Noor <nasir@nasir.id>
2025-06-21 16:54:38 +0700

    add firewall 30080 to service

--- gcp/services/firewall/main.tf
+++ gcp/services/firewall/main.tf
@@ -24,4 +24,17 @@ module "firewall-allow-service" {
   target_tags   = local.ssh_target_tags
 }
 
+module "firewall-allow-30080" {
+  source = "git::https://github.com/nasir19noor/terraform.git//gcp/modules/firewall"
+
+  project       = local.project_id
+  name          = "allow-service port 30080"
+  network       = local.network
+  description   = "allow service container port"
+  protocol      = local.ssh_protocol
+  ports         = ["30000-33000"]
+  source_ranges = local.ssh_source_ranges
+  target_tags   = "gke-nasir-cluster-0c073d54-node"
+}
+
 
