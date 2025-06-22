f76db9d492119a282cd88e0a94fa0dd7f268b372
Nasir Noor <nasir@nasir.id>
2025-06-22 21:54:45 +0700

    update application

--- apps/gke-django/gke_project/gke_app/views.py
+++ apps/gke-django/gke_project/gke_app/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello from Django deployed on GKE, Nasir!")
+    return HttpResponse("Hello from Django deployed on GKE, Nasir! Update")
