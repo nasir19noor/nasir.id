551cd9a363a5f0e8f4f7e50339a90a90048bc4e3
Nasir Noor <nasir19noor@gmail.com>
2025-12-31 20:52:46 +0700

    update

--- gcp/bootstrap/terraform-state-bucket/locals.tf
+++ gcp/bootstrap/terraform-state-bucket/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  config        = yamldecode(file("../config.yaml"))
+  config        = yamldecode(file("../../config.yaml"))
   region        = local.config.global.region
   project_id    = local.config.global.project_id
   bucket_name   = local.config.global.state_bucket
