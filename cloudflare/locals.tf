75dcca97594fe4076f1516f17ae259dd9672e5c3
Nasir Noor <nasir19noor@gmail.com>
2026-01-10 11:28:12 +0700

    update

--- cloudflare/locals.tf
+++ cloudflare/locals.tf
@@ -2,7 +2,7 @@ locals {
   config                  = yamldecode(file("../config.yaml"))
   region                  = local.config.aws.global.region
   bucket                  = local.config.aws.global.state_bucket
-  zone_id                 = local.config.cloudflare.zone_id
+  zone_name               = local.config.cloudflare.zone_name
   root                    = local.config.cloudflare.root
   contabo_ip              = local.config.contabo.ip
 }    
