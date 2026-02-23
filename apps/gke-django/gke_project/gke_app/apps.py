fac0548ffa2c4765b3a58d573367c51a878a75c6
Nasir Noor <nasir19noor@gmail.com>
2026-02-23 11:20:53 +0700

    update

--- apps/gke-django/gke_project/gke_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GkeAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gke_app'
--- apps/gke-django/gke_project/gke_app/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello from Django deployed on GKE, Nasir! Update lagi aja")
