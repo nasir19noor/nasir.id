de93dcfe7315a07ffc6a11a8431d55ec91ba6462
Nasir Noor <nasir19noor@gmail.com>
2026-03-03 11:22:49 +0700

    update

--- apps/iTung/frontend/run.sh
+++ apps/iTung/frontend/run.sh
@@ -4,3 +4,5 @@ docker build \
   --build-arg NEXT_PUBLIC_GOOGLE_CLIENT_ID=943652190384-emo9evfro4d5n6rpabn1artu2brmtm9m.apps.googleusercontent.com \
   -t itung-frontend:latest .
 docker run -d -p 5000:5000 --name itung-frontend itung-frontend:latest
+
+
