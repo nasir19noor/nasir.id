c939f517e94d75305de3fbd4e2b2203c67c97a99
Nasir Noor <nasir19noor@gmail.com>
2026-03-05 20:35:13 +0700

    update bucket www.nasir.id

--- aws/services/s3/main.tf
+++ aws/services/s3/main.tf
@@ -48,15 +48,32 @@ module "s3_nasir" {
   source = "git::https://github.com/nasir19noor/terraform.git//aws/modules/s3"
   bucket = "www.nasir.id"
 
-  # Disable all public access blocking to allow public access
+  # Keep public access blocks disabled to allow the bucket policy below
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 
-  # --- Enable Website and Public Access Features ---
   enable_website_hosting    = true
-  enable_public_read_access = true
+  # Disable blanket public read; a prefix-scoped policy is applied below
+  enable_public_read_access = false
+}
+
+resource "aws_s3_bucket_policy" "nasir_uploads_public_read" {
+  bucket = module.s3_nasir.s3_bucket_id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadUploads"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${module.s3_nasir.s3_bucket_arn}/uploads/*"
+      }
+    ]
+  })
 }
 
 
