cedae76d468b894a8ba93b94e6945ead24ccca39
Nasir Noor <nasir19noor@gmail.com>
2026-03-06 13:20:22 +0700

    update acm assets.nasir.id

--- aws/services/acm/locals.tf
+++ aws/services/acm/locals.tf
@@ -1,9 +1,7 @@
 locals {
   config                  = yamldecode(file("../../../config.yaml"))
   region                  = local.config.aws.global.region
-  domain_name_assets      = local.config.aws.acm.domain_name_assets
-  domain_name             = local.config.aws.acm.domain_name_assets
-  domain_name_nasir       = local.config.aws.cloudfront.domain_name_nasir
+  domain_name_nasir       = local.config.aws.acm.domain_name_nasir
   create_route53_records  = local.config.aws.acm.create_route53_records
   validation_method       = local.config.aws.acm.validation_method
 }    
