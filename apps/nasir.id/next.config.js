6c049bdbef0d55173167cfc1ce5d3e939baadf21
Nasir Noor <nasir19noor@gmail.com>
2026-03-06 19:17:02 +0700

    solving image issue

--- apps/nasir.id/next.config.js
+++ apps/nasir.id/next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 's3.ap-southeast-1.amazonaws.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'assets.nasir.id',
+            },
         ],
     },
 };
