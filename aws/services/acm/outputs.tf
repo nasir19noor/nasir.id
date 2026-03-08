3e8fe55ac047aee33b574c50e24514ad282db867
Nasir Noor <nasir19noor@gmail.com>
2026-03-09 00:32:09 +0700

    add output acm itung.nasr.id

--- aws/services/acm/outputs.tf
+++ aws/services/acm/outputs.tf
@@ -1,7 +1,7 @@
-# output "acm_assets_certificate_arn" {
-#   description = "ARN of the ACM certificate for the assets domain (used by CloudFront)"
-#   value       = module.acm_assets.acm_certificate_arn
-# }
+output "acm_itung_nasir_certificate_arn" {
+  description = "ARN of the ACM certificate for the assets.itung.nasir.id domain (used by CloudFront)"
+  value       = module.acm_itung_nasir.acm_certificate_arn
+}
 
 output "acm_nasir_certificate_arn" {
   description = "ARN of the ACM certificate for the nasir domain (used by CloudFront)"
