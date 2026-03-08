7d051783debbceda64170224c74f6ff0a1c96f40
Nasir Noor <nasir19noor@gmail.com>
2026-03-09 00:05:43 +0700

    add acm assets.itung.nasir.id

--- aws/services/acm/main.tf
+++ aws/services/acm/main.tf
@@ -15,20 +15,19 @@ module "acm_nasir" {
   wait_for_validation    = false
 }
 
-# module "acm_assets" {
-#   source = "git::https://github.com/nasir19noor/terraform.git//aws/modules/acm"
+module "acm_itung_nasir" {
+  source = "git::https://github.com/nasir19noor/terraform.git//aws/modules/acm"
 
-#   providers = {
-#     aws = aws.acm_provider
-#   }
+  providers = {
+    aws = aws.acm_provider
+  }
 
-#   domain_name = local.domain_name_assets
-#   subject_alternative_names = [
-#     "${local.domain_name_assets}"
-#   ]
+  domain_name = local.domain_name_itung_nasir
+  subject_alternative_names = [
+    local.domain_name_nasir
+  ]
 
-#   create_route53_records = local.create_route53_records
-#   validation_method      = local.validation_method
-#   wait_for_validation    = false
-# }
-
+  create_route53_records = local.create_route53_records
+  validation_method      = local.validation_method
+  wait_for_validation    = false
+}
