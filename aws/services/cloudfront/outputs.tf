1a4c312160ee9e9f31202455685d850abf04e285
Nasir Noor <nasir19noor@gmail.com>
2026-03-09 11:37:13 +0700

    add cloudfront output for itung

--- aws/services/cloudfront/outputs.tf
+++ aws/services/cloudfront/outputs.tf
@@ -1,7 +1,7 @@
-# output "cloudfront_itung_distribution_arn" {
-#   description = "ARN of the itung CloudFront distribution (used for OAC bucket policy on assets.itung.nasir.id)"
-#   value       = module.cloudfront_itung.cloudfront_distribution_arn
-# }
+output "cloudfront_itung_distribution_arn" {
+  description = "ARN of the itung CloudFront distribution (used for OAC bucket policy on assets.itung.nasir.id)"
+  value       = module.cloudfront_itung.cloudfront_distribution_arn
+}
 
 output "cloudfront_nasir_distribution_arn" {
   description = "ARN of the nasir CloudFront distribution (used for OAC bucket policy on www.nasir.id)"
