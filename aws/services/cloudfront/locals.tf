371f66908a94cdb32842ebfcc8260795cbc24718
Nasir Noor <nasir19noor@gmail.com>
2026-04-02 00:33:50 +0700

    cf pulsara

--- aws/services/cloudfront/locals.tf
+++ aws/services/cloudfront/locals.tf
@@ -3,5 +3,6 @@ locals {
   region                  = local.config.aws.global.region
   domain_name_itung       = local.config.aws.cloudfront.domain_name_itung
   domain_name_nasir       = local.config.aws.cloudfront.domain_name_nasir
+  domain_name_pulsara     = local.config.aws.cloudfront.domain_name_pulsara
 }    
 
