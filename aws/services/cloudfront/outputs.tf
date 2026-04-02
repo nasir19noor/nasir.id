12f92d07ac37d35348d22b6992a6f3a7fe6d6e7d
Nasir Noor <nasir19noor@gmail.com>
2026-04-02 07:34:30 +0700

    update outputs cloudfront

--- aws/services/cloudfront/outputs.tf
+++ aws/services/cloudfront/outputs.tf
@@ -7,3 +7,8 @@ output "cloudfront_nasir_distribution_arn" {
   description = "ARN of the nasir CloudFront distribution (used for OAC bucket policy on www.nasir.id)"
   value       = module.cloudfront_nasir.cloudfront_distribution_arn
 }
+
+output "cloudfront_pulsara_distribution_arn" {
+  description = "ARN of the pulsaraCloudFront distribution (used for OAC bucket policy on assets.itung.nasir.id)"
+  value       = module.cloudfront_pulsara.cloudfront_distribution_arn
+}
