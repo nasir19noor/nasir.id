c956e7b405681e71de41c0d1c41c225e4faab2b0
Nasir Noor <nasir19noor@gmail.com>
2026-05-21 20:17:08 +0700

    jakpro.nasir.id

--- cloudflare/dns-jakpro.tf
+++ cloudflare/dns-jakpro.tf
@@ -1,3 +1,12 @@
+resource "cloudflare_record" "jakpro_nasir_id_certificate" {
+  zone_id = data.cloudflare_zones.nasir_id.zones[0].id
+  name    = "_acme-challenge_g6gykr3nnl4j3xae.jakpro.nasir.id."
+  content = "0bfdec66-281b-408c-b034-3d4b542575bd.0.asia-southeast2.authorize.certificatemanager.goog."
+  type    = "CNAME"
+  proxied = false
+  ttl     = 300
+}
+
 resource "cloudflare_record" "jppeka1_jakpro_nasir_id" {
   zone_id = data.cloudflare_zones.nasir_id.zones[0].id
   name    = "jppeka1.jakpro.nasir.id"
