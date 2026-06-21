240e1ca4411414ee1d20e41d5e71ab82d7ea947b
Nasir Noor <nasir19noor@gmail.com>
2026-06-21 17:32:35 +0700

    update custom domain api gateway shortener s.nasir.id

--- aws/services/api-gateway/main-shortener.tf
+++ aws/services/api-gateway/main-shortener.tf
@@ -15,6 +15,21 @@ data "aws_api_gateway_rest_api" "shortener" {
   name = module.shortener_api.api_name # ties the lookup to the module so it reads after creation
 }
 
+resource "aws_api_gateway_domain_name" "shortener" {
+  domain_name              = "s.nasir.id"
+  regional_certificate_arn = "arn:aws:acm:ap-southeast-1:647459380434:certificate/66d0a5e7-d310-4df5-8ce6-c07eb0039235"
+  security_policy          = "TLS_1_2"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "shortener" {
+  api_id      = module.shortener_api.api_id
+  stage_name  = aws_api_gateway_stage.this.stage_name
+  domain_name = aws_api_gateway_domain_name.shortener.domain_name
+}
+
 locals {
   rest_api_id       = module.shortener_api.api_id
   root_resource_id  = data.aws_api_gateway_rest_api.shortener.root_resource_id
