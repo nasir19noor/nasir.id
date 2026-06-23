07abd3d6a1582ec372c390283ad210ae4aee0d71
Nasir Noor <nasir19noor@gmail.com>
2026-06-23 08:49:05 +0700

    se bucket policy mbg.nasir.id

--- aws/services/s3/main-mbg.tf
+++ aws/services/s3/main-mbg.tf
@@ -3,12 +3,44 @@ module "s3_mbg" {
   bucket = local.bucket_name_mbg
 
   # Disable all public access blocking to allow public access
-  block_public_acls       = false
-  block_public_policy     = false
+  block_public_acls       = true
+  block_public_policy     = true
   ignore_public_acls      = false
   restrict_public_buckets = false
 
   # --- Enable Website and Public Access Features ---
   enable_website_hosting    = true
   enable_public_read_access = true
+}
+
+
+data "cloudflare_ip_ranges" "cloudflare" {}
+
+data "aws_iam_policy_document" "mbg_cloudflare_only" {
+  statement {
+    sid    = "AllowCloudflareOnly"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.mbg.arn}/*"]
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values = concat(
+        data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks,
+        data.cloudflare_ip_ranges.cloudflare.ipv6_cidr_blocks,
+      )
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "mbg" {
+  bucket = aws_s3_bucket.mbg.id
+  policy = data.aws_iam_policy_document.mbg_cloudflare_only.json
 }
