a3f3161e6c20df8a59a7f759cb55208f6a30e9f7
Nasir Noor <nasir19noor@gmail.com>
2026-07-03 08:48:02 +0700

    update mbg

--- aws/services/dynamodb/main-mbg.tf
+++ aws/services/dynamodb/main-mbg.tf
@@ -1,17 +1,17 @@
-locals {
-  tags = {
-    Project     = "mbg"
-    Environment = "prd"
-    ManagedBy   = "terraform"
-  }
-}
+# locals {
+#   tags = {
+#     Project     = "mbg"
+#     Environment = "prd"
+#     ManagedBy   = "terraform"
+#   }
+# }
 
-module "shortener_table" {
+module "mbg_table" {
   source = "git::https://github.com/nasir19noor/terraform.git//aws/modules/dynamodb?ref=main"
 
   table_name = local.name-mbg
   hash_key   = "shortCode"
-  tags       = local.tags
+  # tags       = local.tags
 }
 
 
