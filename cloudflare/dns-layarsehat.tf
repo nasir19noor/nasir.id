edecb66c9ae2547a4d90cbdaf4c19e5799c8a833
Nasir Noor <nasir19noor@gmail.com>
2026-08-08 22:48:19 +0700

    cloudflare layarsehat

--- cloudflare/dns-layarsehat.tf
+++ cloudflare/dns-layarsehat.tf
@@ -1,15 +1,15 @@
-resource "cloudflare_record" "agent_nasir_id" {
+resource "cloudflare_record" "layarsehat_nasir_id" {
   zone_id = data.cloudflare_zones.nasir_id.zones[0].id
-  name    = "agent.nasir.id"
+  name    = "layarsehat.nasir.id"
   content = "207.180.248.214"
   type    = "A"
   proxied = true
   ttl     = 1
 }
 
-resource "cloudflare_record" "api_agent_nasir_id" {
+resource "cloudflare_record" "api_layarsehat_nasir_id" {
   zone_id = data.cloudflare_zones.nasir_id.zones[0].id
-  name    = "api.agent.nasir.id"
+  name    = "api.layarsehat.nasir.id"
   content = "207.180.248.214"
   type    = "A"
   proxied = false
