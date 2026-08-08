56dabc6134c942e5f5b48430cb48d4999716b20d
Nasir Noor <nasir19noor@gmail.com>
2026-08-09 00:28:56 +0700

    create cognito layarsehat

--- aws/services/cognnito/locals.tf
+++ aws/services/cognnito/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  config                  = yamldecode(file("../../../config.yaml"))
-  region                  = local.config.aws.global.region
-  cognito_name_mbg        = local.config.aws.cognito.cognito_name_mbg
+  config                   = yamldecode(file("../../../config.yaml"))
+  region                   = local.config.aws.global.region
+  cognito_name_layarsehat  = local.config.aws.cognito.cognito_name_layarsehat
 }    
 
