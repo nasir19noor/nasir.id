205d3add50fc5c6fa083b444fdce29a1f8c48860
Nasir Noor <nasir19noor@gmail.com>
2026-08-19 01:13:47 +0700

    create subnets

--- aws/services/subnets/main.tf
+++ aws/services/subnets/main.tf
@@ -1,6 +1,7 @@
 locals {
   cidr_block  = data.terraform_remote_state.vpc.outputs.vpc_cidr_block
   name        = data.terraform_remote_state.vpc.outputs.vpc_name
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
   public_subnet_cidrs  = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   private_subnet_cidrs = ["10.1.11.0/24", "10.1.12.0/24", "10.1.13.0/24"]
 }
